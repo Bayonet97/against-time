86bfeeb4e15268592090a2fe2248c051df2113e8
Stef B <s.boelens@student.fontys.nl>
2019-02-21 12:14:51 +0100

    3D character, movement oriented
    BOOM

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public GameObject playerObject;
+
     // Normal Movements Variables
-    private float walkSpeed;
+    public float walkSpeed;
     private float curSpeed;
     private float maxSpeed;
 
-   // private CharacterStat plStat;
+    // private CharacterStat plStat;
 
     void Start()
     {
@@ -17,16 +19,65 @@ public class PlayerMovement : MonoBehaviour
 
         // walkSpeed = (float)(plStat.Speed + (plStat.Agility / 5));
         //  sprintSpeed = walkSpeed + (walkSpeed / 2);
-        walkSpeed = 5;
+        walkSpeed = 0.5f;
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        curSpeed = walkSpeed;
-        maxSpeed = curSpeed;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
+            rotatePlayerObjectY(0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+            rotatePlayerObjectY(270);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * walkSpeed * Time.deltaTime);
+            rotatePlayerObjectY(180);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * walkSpeed * Time.deltaTime);
+            rotatePlayerObjectY(90);
+        }
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
+        {
+            rotatePlayerObjectY(315);
+        }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            rotatePlayerObjectY(45);
+        }
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
+        {
+            rotatePlayerObjectY(135);
+        }
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            rotatePlayerObjectY(225);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+            walkSpeed = 3f;
+        else
+            walkSpeed = 1f;
+    }
 
-        // Move senteces
-        GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * curSpeed, 0.8f),
-                                             Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
+    //void FixedUpdate()
+    //{
+    //    curSpeed = walkSpeed;
+    //    maxSpeed = curSpeed;
+
+    //    // Move senteces
+    //    GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f),
+    //                                         Mathf.Lerp(0, Input.GetAxis("Vertical") * curSpeed, 0.8f));
+    //}
+
+    private void rotatePlayerObjectY(int yAxis)
+    {
+        playerObject.transform.eulerAngles = new Vector3(0, yAxis, 0);
     }
 }

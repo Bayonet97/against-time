5d0b6007f31b59f3929237c15b809bf8dc4c8683
Akkerveken,Raphaël R. van <r.vanakkerveken@student.fontys.nl>
2019-02-21 14:44:29 +0100

    Added timer with MV(c)

---
+++ Assets/Scripts/ScriptableObjects/TimerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public float deathTimer = 60f;
+    public Text timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deathTimer -= Time.deltaTime;
+        timerText.text = deathTimer.ToString();
+        if (deathTimer < 0)
+        {
+            timerText.text = "Time's up.";
+        }
+    }
+}
